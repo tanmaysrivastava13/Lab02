94a0d56e888bac11d883ec4ce033aec9db367c9a
tanmaysrivastava <tanmay.srivastava_cs18@gla.ac.in>
2019-08-22 15:00:19 +0530

    add file Main.java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+
+/**
+ * @author VISHWAS DUBEY
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String s = obj.nextLine();
+        System.out.printIn("Welcome," + s);
+        Scanner.close();
+    }
+}
+{
+        }
